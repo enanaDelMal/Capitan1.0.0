d2193c8e608b39567750e9aeeda8e60e71dd9d30
Daguiana <darevolledo@hotmail.com>
2017-03-06 17:23:30 -0500

    botones

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+
+
+var theSprint1 = document.getElementById('sprint-1');
+  theSprint1.onclick = function() {
+    var datos = document.getElementById('relleno');
+    var relleno= relleno.innerHTML = ("Sprint 1: Introduccion a Javascitp");
+};
+
+
+//var theSprint2 = document.getElementById('sprint-2');
+  //  theSprint.onclick = function() {
+//var theSprint3 = document.getElementById('sprint-3');
+  //  theSprint.onclick = function(){
